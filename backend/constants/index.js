24711d3649c5cb3c2faebeafabf3ab1e75192559
ldagar315 <78065479+ldagar315@users.noreply.github.com>
2022-08-09 10:44:29 +0530

    First Commit

---
+++ backend/constants/index.js
@@ -0,0 +1,8 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x82075bf2856541CD9521fb6f3b81a8Ac735CBd8B";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+// This contracts address = "0xA7Ce12D6De424c58896d7f8Eeade961d30a519A1"
